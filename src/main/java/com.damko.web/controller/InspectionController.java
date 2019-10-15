963068bc9bfb8f0a9e9b9e65cc5b6549d4aa984e
damianKokot <kokocik1213@gmail.com>
2019-10-15 19:38:57 +0200

    Added controller and got Spring

---
+++ src/main/java/com.damko.web/controller/InspectionController.java
@@ -0,0 +1,15 @@
+package com.damko.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InspectionController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Witam serdecznie na sprawdzaniu mojego programu!</h2>
+</body>
+</html>
